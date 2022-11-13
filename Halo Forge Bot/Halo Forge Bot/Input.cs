4da49f3faaf19b048bfccb97e1fea560b3251fa6
joshf67 <iownall99@live.co.uk>
2022-11-13 22:23:34 +0000

    Added Managed WinAPI for LLHooks
    Used for listening to mouse clicks

--- Halo Forge Bot/Halo Forge Bot/Input.cs
+++ Halo Forge Bot/Halo Forge Bot/Input.cs
@@ -1,8 +1,41 @@
+using System;
+using System.Threading.Tasks;
+using ManagedWinapi.Hooks;
 using WindowsInput;
+using WindowsInput.Native;
+
 
 namespace ForgeMacros;
 
 public static class Input
 {
     public static InputSimulator Simulate = new InputSimulator();
+    public static LowLevelMouseHook MouseHook = new LowLevelMouseHook();
+    public static LowLevelKeyboardHook KeyboardHook = new LowLevelKeyboardHook();
+    public static bool InputActive = false;
+
+    public static void InitInput()
+    {
+        InputActive = true;
+        if (!MouseHook.Hooked) MouseHook.StartHook();
+        if (!KeyboardHook.Hooked) KeyboardHook.StartHook();
+        KeyboardHook.KeyIntercepted +=
+            (int msg, int code, int scanCode, int flags, int time, IntPtr info, ref bool handled) =>
+            {
+                if (code == (int)VirtualKeyCode.ESCAPE)
+                {
+                    MouseHook.Dispose();
+                    KeyboardHook.Dispose();
+                    throw new Exception($"Implement proper exit here");
+                }
+            };
+    }
+
+    public static void WaitForInput(VirtualKeyCode key)
+    {
+        while (!Simulate.InputDeviceState.IsHardwareKeyDown(key))
+        {
+            Simulate.Keyboard.Sleep(10);
+        }
+    }
 }
