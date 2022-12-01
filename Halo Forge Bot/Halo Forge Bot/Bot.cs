470a959a73aa18e98eed3f5db55cc79c4ceb90e5
joshf67 <iownall99@live.co.uk>
2022-12-01 02:01:28 +0000

    Moved StartBot into sub-functions

--- Halo Forge Bot/Halo Forge Bot/Bot.cs
+++ Halo Forge Bot/Halo Forge Bot/Bot.cs
@@ -34,10 +34,9 @@ namespace Halo_Forge_Bot;
 
 public static class Bot
 {
-    public static int travelSleep = 10;
-
     public static int WhenToSave = 15;
     //todo extract all NON BOT LOGIC for start bot, it should only be for starting / ending the bot
+    
     public static async Task StartBot(List<ForgeItem> map, int itemStart = 0, int itemEnd = 0,
         bool resumeFromLast = false , bool isBlender = false)
     {
@@ -50,7 +49,15 @@ public static class Bot
 
         // LoadItemData();
         BuildUILayout();
+        
+        BotSetup(map, out startIndex, itemStart, itemEnd, resumeFromLast, ref items);
+        await BotLoop(items, startIndex, resumeFromLast, isBlender);
+    }
 
+    private static void BotSetup(List<ForgeItem> map, out int startIndex, int itemStart, int itemEnd,
+        bool resumeFromLast, ref Dictionary<ObjectId, List<MapItem>> items)
+    {
+        startIndex = 0;
         if (resumeFromLast)
         {
             var recoveryValues = GetRecoveryFiles();
@@ -68,6 +75,7 @@ public static class Bot
             var tempArray =
                 map.OrderBy(item => item.ItemId)
                     .ToList(); // temp to an array to i know now for sure its in the correct order. might be unnecessary 
+            
             if (itemEnd == 0)
             {
                 itemEnd = tempArray.Count();
@@ -110,116 +118,27 @@ public static class Bot
 
             WriteObjectRecoveryFile(items);
         }
+    }
 
+    private static async Task BotLoop(Dictionary<ObjectId, List<MapItem>> items, int startIndex, bool resumeFromLast, bool isBlender)
+    {
         ForgeUI.SetHaloProcess();
         int itemCountID = 0;
         int saveCount = 0;
 
         foreach (var item in items)
         {
-            /*
-            //todo extract all the data processing and the bot logic from each other
-            // while
-            //     (MemoryHelper.GetGlobalHover() !=
-            //      0) // reset the cursor to the top of the current menu (in most cases the object browser)
-            // {
-            //     Input.Simulate.Keyboard.KeyPress(VirtualKeyCode.VK_W);
-            //     await Task.Delay(travelSleep);
-            // }
-
-            await NavigationHelper.ReturnToTop();
-
-            await Task.Delay(200);
-
-            // while (MemoryHelper.GetTopBrowserHover() != 0) // set item browser to active menu
-            // {
-            //     Input.Simulate.Keyboard.KeyPress(VirtualKeyCode.VK_E);
-            //     await Task.Delay(travelSleep);
-            // }
-            
-            // set item browser to active menu
-            await NavigationHelper.MoveToTab(0);
-
-            var currentObjectId = item.Key;
-
-            ForgeObjectBrowser.FindItem(currentObjectId, out ForgeUIObject? mapitem); // gets the items ui location data
-
-            if (mapitem == null)
-            {
-                Log.Warning("Skipping null item, MapId: {id}, Name: {name} ", Enum.GetName(currentObjectId));
-                continue;
-            }
-
-            //navigate to item with memory checks
-            // while (MemoryHelper.GetGlobalHover() !=
-            //        mapitem.ParentFolder.ParentCategory.CategoryOrder - 1) //Set cursor to correct cat
-            // {
-            //     Log.Debug("Move To Cat currentHover:{Hover} , Required Hover: {Reqired}",
-            //         MemoryHelper.GetGlobalHover(), mapitem.ParentFolder.ParentCategory.CategoryOrder - 1);
-            //     Input.Simulate.Keyboard.KeyPress(VirtualKeyCode.VK_S);
-            //
-            //     await Task.Delay(travelSleep);
-            // }
-
-            await NavigationHelper.NavigateVertical(mapitem.ParentFolder.ParentCategory.CategoryOrder);
-
-            await Task.Delay(200);
-            Input.Simulate.Keyboard.KeyPress(VirtualKeyCode.RETURN); // open the cat
-            await Task.Delay(200);
-
-
-            // while (MemoryHelper.GetGlobalHover() !=
-            //        mapitem.ParentFolder.ParentCategory.CategoryOrder +
-            //        mapitem.ParentFolder.FolderOffset - 1) // move cursor to sub cat
-            // {
-            //     Log.Debug("Move To subCat currentHover:{Hover} , Required Hover: {Reqired}",
-            //         MemoryHelper.GetGlobalHover(),
-            //         mapitem.ParentFolder.ParentCategory.CategoryOrder +
-            //         mapitem.ParentFolder.FolderOffset - 1);
-            //     Input.Simulate.Keyboard.KeyPress(VirtualKeyCode.VK_S);
-            //
-            //     await Task.Delay(travelSleep);
-            // }
-
-            await NavigationHelper.NavigateVertical(mapitem.ParentFolder.ParentCategory.CategoryOrder +
-                                                    mapitem.ParentFolder.FolderOffset);
-
-            await Task.Delay(200);
-            Input.Simulate.Keyboard.KeyPress(VirtualKeyCode.RETURN); // enter the sub cat
-            await Task.Delay(200);
-
-
-            // while (MemoryHelper.GetGlobalHover() != mapitem.ObjectOrder - 1) // hover item
-            // {
-            //     Input.PressKey(VirtualKeyCode.VK_S);
-            //     await Task.Delay(travelSleep);
-            // }
-
-            await NavigationHelper.NavigateVertical(mapitem.ObjectOrder);
-            */
-            
             ForgeUIObject _forgeObject;
             ForgeObjectBrowser.FindItem(item.Key, out _forgeObject);
 
-            foreach (var mapItem in item.Value) // the start of the item spawning loop
+            foreach (var mapItem in item.Value.Where(mapItem => mapItem.UniqueId >= startIndex || !resumeFromLast))
             {
-                if (mapItem.UniqueId < startIndex && resumeFromLast)
-                {
-                    continue;
-                }
-
                 WriteObjectRecoveryIndexToFile(mapItem.UniqueId);
 
                 saveCount++;
                 await Task.Delay(200);
-                
+
                 await NavigationHelper.SpawnItem(_forgeObject);
-                
-                // while (MemoryHelper.GetMenusVisible() == 1)
-                // {
-                //     Input.Simulate.Keyboard.KeyPress(VirtualKeyCode.RETURN); // spawn the item?
-                //     await Task.Delay(200);
-                // }
 
                 await Task.Delay(200);
 
@@ -232,69 +151,17 @@ public static class Bot
                     await Task.Delay(100);
                 }
 
-                /*
-                while (MemoryHelper.GetEditMenuState() == 0)
-                {
-                    Input.Simulate.Keyboard.KeyPress(VirtualKeyCode.F2);
-                }
-
-                await Task.Delay(75);
-
-                var newName = $"{Enum.GetName((ObjectId)itemSchema.ItemId.Int)}--{itemCountID}--";
-                await ClipboardService.SetTextAsync(newName);
-                while (await ClipboardService.GetTextAsync() != newName)
-                {
-                    await ClipboardService.SetTextAsync(newName);
-                }
-
-                await Task.Delay(10);
-                while (MemoryHelper.GetEditBoxText() != newName)
-                {
-                    Input.Simulate.Keyboard.ModifiedKeyStroke(VirtualKeyCode.CONTROL, VirtualKeyCode.VK_A);
-                    await Task.Delay(10);
-                    Input.Simulate.Keyboard.KeyPress(VirtualKeyCode.BACK);
-                    await Task.Delay(10);
-                    Input.Simulate.Keyboard.ModifiedKeyStroke(VirtualKeyCode.CONTROL, VirtualKeyCode.VK_V);
-                    await Task.Delay(10);
-                    Input.Simulate.Keyboard.KeyPress(VirtualKeyCode.RETURN);
-                }
-
-
-                while (MemoryHelper.GetMenusVisible() != 0)
-                {
-                    Input.Simulate.Keyboard.KeyPress(VirtualKeyCode.RETURN);
-                    await Task.Delay(10);
-                }
-                */
-
-                await PropertyHelper.SetMainProperties(mapItem.item,isBlender);
+                await PropertyHelper.SetMainProperties(mapItem.item, _forgeObject.DefaultObjectMode == ForgeUIObjectModeEnum.NONE ? ForgeUIObjectModeEnum.STATIC_FIRST : _forgeObject.DefaultObjectMode, isBlender);
                 itemCountID++;
             }
-            /*
-            await Task.Delay(75);
-           // Input.Simulate.Keyboard.KeyPress(VirtualKeyCode.BACK);
-            await Task.Delay(100);
-            
-            // while (MemoryHelper.GetGlobalHover() != mapitem.ParentFolder.ParentCategory.CategoryOrder - 1)
-           
-            while (MemoryHelper.GetGlobalHover() != _forgeObject.ParentFolder.ParentCategory.CategoryOrder - 1)
-            {
-                Input.Simulate.Keyboard.KeyPress(VirtualKeyCode.VK_W);
-                await Task.Delay(travelSleep);
-            }
-
-            await Task.Delay(100);
-            Input.Simulate.Keyboard.KeyPress(VirtualKeyCode.RETURN);
-            await Task.Delay(500);
-            // Input.Simulate.Keyboard
-
-            //MemoryHelper.SetGlobalHover(UIItems[currentObjectId].index);
-            //  MemoryHelper.SetBrowserScroll(UIItems[currentObjectId].index);
-            */
         }
     }
 
+    /// <summary>
+    /// Ensures the UI layout is only built once
+    /// </summary>
     private static bool UILayoutBuilt = false;
+    
     /// <summary>
     /// This needs changing to work with items that seem to be getting removed (like the last few antennas)
     /// </summary>
@@ -316,7 +183,7 @@ public static class Bot
 
             ForgeObjectBrowser.AddItem(ConformForgeObjects.FixCapital(objectData[0].ToLower()),
                 ConformForgeObjects.FixCapital(objectData[1].ToLower()),
-                Enum.GetName((ObjectId)int.Parse(objectData[3])) ?? objectData[2], Enum.Parse<ObjectId>(objectData[3]));
+                Enum.GetName((ObjectId)int.Parse(objectData[3])) ?? objectData[2], Enum.Parse<ObjectId>(objectData[3]), ForgeUIObjectModeEnum.STATIC_FIRST);
         }
     }
 
