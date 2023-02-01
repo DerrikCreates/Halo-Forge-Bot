c477e0b3f7b717a5345309ff40ece51eb619ac85
Derrik <heyimderrik@gmail.com>
2023-02-01 05:24:03 -0500

    Update BotSettings.cs

--- Halo Forge Bot/Halo Forge Bot/Core/BotSettings.cs
+++ Halo Forge Bot/Halo Forge Bot/Core/BotSettings.cs
@@ -7,6 +7,7 @@ public class BotSettings
     public int ItemStartIndex, ItemEndIndex;
     public float Scale = 1;
     public Vector3 Offset = Vector3.Zero;
+    public Vector3 NewCenter = new Vector3(0, 0, 60);
     public int SaveFrequency = 100;
     public bool RecenterMap = false;
 }
